10b01e6508801da18f427b9ba9021f3c4dc378b9
Felix Nievelstein <fn@impacgroup.de>
2019-06-11 17:51:46 +0200

    adding activity for android

---
+++ src/android/IMPSwissRxActivity.java
@@ -0,0 +1,13 @@
+package de.impacgroup.swissrxlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IMPSwissRxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_impswiss_rx);
+    }
+}
--- src/android/SwissRxLogin.java
+++ src/android/SwissRxLogin.java
@@ -1,27 +1,54 @@
+
 package de.impacgroup.swissrxlogin;
 
 
-import android.util.Log;
+import android.content.Context;
+import android.content.Intent;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import static org.apache.cordova.Whitelist.TAG;
-
 public class SwissRxLogin extends CordovaPlugin {
+
+    private static final String CONST_APPID= "appId";
+    private static final String CONST_COMPANYID 	= "companyId";
+
+    private CallbackContext callbackContext;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("showLogin")) {
-            Log.d(TAG, "execute: " + action);
-            return super.execute(action, args, callbackContext);
+            showLogin(args.getString(1), args.getString(0));
+            this.callbackContext = callbackContext;
+            return true;
         } else {
             callbackContext.error("\"" + action + "\" is not a recognized action.");
             return false;
         }
 
+    }
+
+    private void showLogin(final String companyID, final String appId) {
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                Context context = cordova.getActivity().getApplicationContext();
+                cordova.setActivityResultCallback(SwissRxLogin.this);
+                Intent intent = new Intent(context, IMPSwissRxActivity.class);
+                intent.putExtra(CONST_APPID, appId);
+                intent.putExtra(CONST_COMPANYID, companyID);
+                cordova.getActivity().startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        callbackContext.success(0);
+        super.onActivityResult(requestCode, resultCode, intent);
     }
 }
 
