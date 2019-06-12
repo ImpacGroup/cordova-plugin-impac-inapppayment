2ea3d959a62c204ea3d7b2b1b940e21685374f8b
Felix Nievelstein <fn@impacgroup.de>
2019-06-12 09:40:02 +0200

    fix can compile with activity

--- src/android/IMPSwissRxActivity.java
+++ src/android/IMPSwissRxActivity.java
@@ -8,6 +8,6 @@ public class IMPSwissRxActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_impswiss_rx);
+        setContentView(getResources().getIdentifier("activity_impswiss_rx", "layout", getPackageName()));
     }
 }
--- src/android/SwissRxLogin.java
+++ src/android/SwissRxLogin.java
@@ -1,4 +1,3 @@
-
 package de.impacgroup.swissrxlogin;
 
 
